0752af5c5ac51d38a1f71f0b0ddfb9b8910538c5
Marcos Martínez Játiva <marcos.martinez-jativa-external@capgemini.com>
2022-03-24 13:19:22 +0100

    Fixed coverage usage

--- angular/src/karma.conf.js
+++ angular/src/karma.conf.js
@@ -10,14 +10,21 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/angular'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+    coverageReporter: {
+    dir: require('path').join(__dirname, '../coverage/angular'),
+      //reports: ['html', 'lcovonly', 'text-summary', 'lcov'],
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' }
+      ],
       fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
